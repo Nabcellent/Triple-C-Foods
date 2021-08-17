ce06b53b49649e5569dca7015935590b13afcf46
Red-Beard7 <michael.nabangi@strathmore.edu>
2021-08-18 01:27:20 +0300

    Heroku: secure assets with URL forcescheme

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,6 +27,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if(config('app.env') === 'production') URL::forceScheme('https');
     }
 }
