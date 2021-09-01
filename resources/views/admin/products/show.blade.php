51bd73b7c65da84ab8591e28ca50a1cfa3580dc3
Red-Beard7 <michael.nabangi@strathmore.edu>
2021-09-01 20:11:53 +0300

    Admin kitchen - Other products bug fixed

--- resources/views/admin/products/show.blade.php
+++ resources/views/admin/products/show.blade.php
@@ -75,7 +75,7 @@
                         </div>
                     </div>
                 </div>
-                @if($otherProducts)
+                @if(count($otherProducts))
                     <div class="mt-16">
                         <h3 class="text-gray-600 text-2xl font-medium">Other Products</h3>
                         <div class="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
